17d0dc496824f5c83e79359369687409d2627557
Abhi Devireddy <abhi@devireddy.com>
2018-08-20 21:10:48 -0500

    Working with AJAX search

--- get_info.py
+++ get_info.py
@@ -4,49 +4,52 @@ import getpass
 import json
 import schedule
 import time
+from multiprocessing.dummy import Pool as ThreadPool
 
-def get_info(user, pwd):
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
-    with open("device_list.txt") as file:
-        for sw in file:
-            sw = sw.rstrip('\n')
-            redis_db = redis.Redis(host="localhost")
-            cisco_3850 = {
-                'device_type': 'cisco_ios',
-                'ip':   sw,
-                'username': user,
-                'password': pwd
-            }
-            print("Connecting to " + sw)
-            net_connect = ConnectHandler(**cisco_3850)
-            hostname = net_connect.find_prompt()[:-1]
-            serial = net_connect.send_command("show ver | in ID").split(" ")[-1]
-            arp_table = net_connect.send_command("show ip arp", use_textfsm=True)
-            fdb = net_connect.send_command("show mac address-table", use_textfsm=True)
-            for entry in arp_table:
-                entry["switch"] = hostname
-                entry["switch_ip"] = sw
-                entry["switch_serial"] = serial
-                redis_db.rpush("arp_table_new", json.dumps(entry))
-            if (type(fdb) == type([])):
-                for entry in fdb:
-                    entry["switch"] = hostname
-                    entry["switch_ip"] = sw
-                    entry["switch_serial"] = serial
-                    redis_db.rpush("fdb_new", json.dumps(entry))
-        redis_db.delete("arp_table")
-        redis_db.rename("arp_table_new", "arp_table")
-        redis_db.delete("fdb")
-        redis_db.rename("fdb_new", "fdb")
-
+redis_db = redis.Redis(host="localhost")
+redis_db.flushdb()
 
 user = input("Username:")
 pwd = getpass.getpass("Password:")
 
-# Run get_info every 5 minutes to download the latest info from the switches
-schedule.every(15).minutes.do(get_info, user, pwd)
+def get_info(sw):
+    cisco_3850 = {
+        'device_type': 'cisco_ios',
+        'ip':   sw,
+        'username': user,
+        'password': pwd
+    }
+    print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] Connecting to " + sw)
+    net_connect = ConnectHandler(**cisco_3850)
+    hostname = net_connect.find_prompt()[:-1]
+    serial = net_connect.send_command("show ver | in ID").split(" ")[-1]
+    arp_table = net_connect.send_command("show ip arp", use_textfsm=True)
+    fdb = net_connect.send_command("show mac address-table", use_textfsm=True)
+    for entry in arp_table:
+        entry["switch"] = hostname
+        entry["switch_ip"] = sw
+        entry["switch_serial"] = serial
+        redis_db.rpush("arp:" + entry["mac"] + ":" + entry["address"], json.dumps(entry))
+    if (type(fdb) == type([])):
+        for entry in fdb:
+            entry["switch"] = hostname
+            entry["switch_ip"] = sw
+            entry["switch_serial"] = serial
+            redis_db.rpush("fdb:" + entry["destination_address"], json.dumps(entry))
 
-get_info(user, pwd)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+pool = ThreadPool(20)
+
+sw_list = []
+with open("device_list.txt") as file:
+    for sw in file:
+        sw_list.append(sw.rstrip('\n'))
+pool.map(get_info, sw_list)
+
+redis_db.delete("arp_table")
+redis_db.rename("arp_table_new", "arp_table")
+redis_db.delete("fdb")
+redis_db.rename("fdb_new", "fdb")
+
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
--- server.rb
+++ server.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
+require 'sinatra/json'
 require "sinatra/reloader" if development?
 require 'redis'
 
 $redis = Redis.new
 
 get '/' do
-  @arp = $redis.lrange("arp_table", 0, -1).collect { |x| JSON.parse(x)}
-  @fdb = $redis.lrange("fdb", 0, -1).collect { |x| JSON.parse(x)}
   haml :index
+end
+
+get '/arp' do
+  keys = $redis.keys("arp:*#{params["q"]}*")
+  @arp = []
+  keys.each do |key|
+    @arp << $redis.lrange(key, 0, -1).collect { |x| JSON.parse(x)}
+  end
+  json @arp.flatten!
+end
+
+get '/fdb' do
+  keys = $redis.keys("fdb:*#{params["q"]}*")
+  @fdb = []
+  keys.each do |key|
+    @fdb << $redis.lrange(key, 0, -1).collect { |x| JSON.parse(x)}
+  end
+  json @fdb.flatten!
 end
